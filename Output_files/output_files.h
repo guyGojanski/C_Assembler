dfba04505a34eb6a0348a413dfec2b282324cb95
guygo <guygo141@gmail.com>
2026-05-06 13:03:54 +0300

    Output & QA: Base64 encoding and comprehensive test suites

---
+++ Output_files/output_files.h
@@ -0,0 +1,11 @@
+#ifndef PROJECT_IN_C_OUTPUT_FILES_H
+#define PROJECT_IN_C_OUTPUT_FILES_H
+
+#include "../Assembler/assembler.h"
+
+
+void output_unit(const char *file_name,const struct arrays * array);
+
+
+
+#endif
